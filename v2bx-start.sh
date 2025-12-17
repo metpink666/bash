e4422d4420842d8a25fdc3453c9a6da86f3bae90
metpink <83404655+metpink666@users.noreply.github.com>
2025-12-18 07:42:37 +0800

    Update v2bx-start.sh

--- v2bx-start.sh
+++ v2bx-start.sh
@@ -16,14 +16,14 @@ while true; do
     echo "             [2]:  启动                     "
     echo "             [9]:  退出                     "
     echo "————————————————————————————————————————————"
-    echo -n "     请选择: "
+    echo -n "      请选择: "
     read choice
     
     if [ "$choice" = "1" ]; then
         clear
         echo "状态:"
         ps aux | grep V2bX
-        echo "============================================"
+        echo "————————————————————————————————————————————"
         echo "按回车键返回菜单..."
         read
     elif [ "$choice" = "2" ]; then
@@ -32,7 +32,7 @@ while true; do
         cd /usr/local/V2bX || exit
         nohup ./V2bX server -w=false >/dev/null 2>&1 &
         echo "V2bX 已成功运行于 $(date)"
-        echo "============================================"
+        echo "————————————————————————————————————————————"
         echo "按回车键返回菜单..."
         read
     elif [ "$choice" = "9" ]; then

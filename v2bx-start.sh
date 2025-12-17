54182025a44135986a0281ab24ed4006ba3ffd2b
metpink <83404655+metpink666@users.noreply.github.com>
2025-12-18 07:30:54 +0800

    Update v2bx-start.sh

--- v2bx-start.sh
+++ v2bx-start.sh
@@ -9,18 +9,20 @@ cat << 'EOF' > "$VB_CMD"
 
 while true; do
     clear
-    echo "=== V2bX ==="
-    echo "1. 检测运行状态"
-    echo "2. 启动"
-    echo "99. 退出"
-    echo -n "选择: "
+    echo "====== 泠 の V2bX - lxc - debian 专用 ======"
+    echo "============================================"
+    echo "=========  [ 1]:     检测运行状态  ========="
+    echo "=========  [ 2]:     启动          ========="
+    echo "=========  [99]:     退出          ========="
+    echo "============================================"
+    echo -n "请选择: "
     read choice
     
     if [ "$choice" = "1" ]; then
         clear
         echo "状态:"
         ps aux | grep V2bX
-        echo ""
+        echo "===================================================================================================================================="
         echo "按回车键返回菜单..."
         read
     elif [ "$choice" = "2" ]; then
@@ -28,8 +30,8 @@ while true; do
         pkill -f "V2bX server" 2>/dev/null
         cd /usr/local/V2bX || exit
         nohup ./V2bX server -w=false >/dev/null 2>&1 &
-        echo "V2bX started at $(date)"
-        echo ""
+        echo "V2bX 已成功运行于 $(date)"
+        echo "===================================================================================================================================="
         echo "按回车键返回菜单..."
         read
     elif [ "$choice" = "99" ]; then
